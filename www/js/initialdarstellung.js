62ced66d64b32f1c38ad5de5a265233916034901
deuxmais <48289535+deuxmais@users.noreply.github.com>
2019-06-15 12:27:58 +0200

    Update initialdarstellung.js

--- www/js/initialdarstellung.js
+++ www/js/initialdarstellung.js
@@ -1,110 +1,113 @@
 //Teamcount
-var teamsremaining=0;
-var removereq=0;
-var teamCount=0;
+var teamsremaining = 0;
+var removereq = 0;
+var teamCount = 0;
 //Weiterleitung nach Finalspiel
-function init(){
-	if(0<removereq)
-		{
-			window.setTimeout(init,100);
-			return;
-		}
+function init() {
+    if (0 < removereq) {
+        window.setTimeout(init, 100);
+        return;
+    }
     $("#teams_table").html("<tr><th>Team 1</th><th>Ergebnis</th><th>Team 2</th></tr>");
-	$.ajax({url: "../db/teams.php"}).done(recievedteamnames).fail(failedteamnames);
-    
-    if(teamsremaining==2)
-        {
-            window.location.href = "../html/final_page.html"
-        }
+    $.ajax({
+        url: "../db/teams.php"
+    }).done(recievedteamnames).fail(failedteamnames);
+
+    if (teamsremaining == 2) {
+        window.location.href = "../final_page/index.html"
+    }
 }
 //Teampaarungen
-function recievedteamnames(data){
-	teams=JSON.parse(data);
-        teams.forEach(addTeamName);
+function recievedteamnames(data) {
+    teams = JSON.parse(data);
+    teams.forEach(addTeamName);
 }
-function addTeamName(team,teamCount)
-{
-	if(0==teamCount%2)
-		{
-			jQuery('<tr></tr>', 
-			{
-    			id: "tabrow"+teamCount
-    		})
-			.appendTo("#teams_table");
-			jQuery('<td></td>', 
-			{
-    			id: "tabteam"+teamCount
-    		})
-			.appendTo("#tabrow"+teamCount);
-			$("#tabteam"+teamCount)
-				.html(team.name);
-			jQuery('<input/>', 
-			{
-    			id: "teamleft",
-				team_id: "'"+team.id+"'",
-				type: "number",
-				min: "0",
-				name:"score",
-				style:"width:50px;float:left;",
-				class:"form-control team"
-    		})
-			.appendTo("#tabrow"+teamCount);
-		}
-	else{
-			jQuery('<input/>', 
-			{
-    			id: "teamright",
-				team_id: "'"+team.id+"'",
-				type: "number",
-				min: "0",
-				name:"score",
-				style:"width:50px;float:right;",
-				class:"form-control team"
-    		})
-			.appendTo("#tabrow"+(teamCount-1));
-			jQuery('<td></td>', 
-			{
-    			id: "tabteam"+teamCount
-    		})
-			.appendTo("#tabrow"+(teamCount-1));
-			$("#tabteam"+teamCount)
-				.html(team.name);
-		}
+//Team Tabelle Aufbauen
+function addTeamName(team, teamCount) {
+    // Wenn das Team gerade ist
+    if (0 == teamCount % 2)
+    // wird das html für die Tabelle in der Linken Spalte generiert
+    {
+        jQuery('<tr></tr>', {
+                id: "tabrow" + teamCount
+            })
+            .appendTo("#teams_table");
+        jQuery('<td></td>', {
+                id: "tabteam" + teamCount
+            })
+            .appendTo("#tabrow" + teamCount);
+        $("#tabteam" + teamCount)
+            .html(team.name);
+        jQuery('<input/>', {
+                id: "teamleft",
+                team_id: "'" + team.id + "'",
+                type: "number",
+                min: "0",
+                name: "score",
+                style: "width:50px;float:left;",
+                class: "form-control team"
+            })
+            .appendTo("#tabrow" + teamCount);
+    }
+    // Wenn das Team ungerade ist
+    else {
+        // wird das html für die Tabell in der Rechten Spalte generiert
+        jQuery('<input/>', {
+                id: "teamright",
+                team_id: "'" + team.id + "'",
+                type: "number",
+                min: "0",
+                name: "score",
+                style: "width:50px;float:right;",
+                class: "form-control team"
+            })
+            .appendTo("#tabrow" + (teamCount - 1));
+        jQuery('<td></td>', {
+                id: "tabteam" + teamCount
+            })
+            .appendTo("#tabrow" + (teamCount - 1));
+        $("#tabteam" + teamCount)
+            .html(team.name);
+    }
 }
 //Fehler bei Teams laden
-function failedteamnames(){
-	alert("fail");
+function failedteamnames() {
+    alert("fail");
 }
-//remove Teams
-window.onload=init;
-     $('#teamsubmit').on('click', function (e) {
-          e.preventDefault();
-		 		$("#teams_table").find("tr").each(function(e)
-				{
-                    if ($(this).find("#teamleft").val()>$(this).find("#teamright").val()) {
-				   	removereq++;
-                    teamsremaining++;
-				   $.ajax({url: "../db/teamremove.php?id="+$(this).find("#teamright").attr("team_id")}).done(removereturned).fail(removereturned);
-                    } 
-					else if ($(this).find("#teamleft").val()<$(this).find("#teamright").val()) {
-				   	removereq++;
-                    teamsremaining++;
-				   $.ajax({url: "../db/teamremove.php?id="+$(this).find("#teamleft").attr("team_id")}).done(removereturned).fail(removereturned);
-                    }
-                    else {
-                    teamsremaining++;
-                    }
-               });
-     		init();
-            if (teamsremaining>2)
-                {
-                    teamsremaining=0;
-                }
-            else 
-                {
-                    
-                }
-	 });
-	function removereturned(){
-		removereq--;
-	}
+
+window.onload = init;
+
+// Verlierer Teams entfernen
+$('#teamsubmit').on('click', function (e) {
+    e.preventDefault();
+    // finde alle zeilen in der teams tabelle und loope über diese
+    $("#teams_table").find("tr").each(function (e) {
+        // wenn das linke team höhere punkte als das rechte team hat
+        if ($(this).find("#teamleft").val() > $(this).find("#teamright").val()) {
+            removereq++;
+            teamsremaining++;
+            // wird das rechte team via ajax request entfernt
+            $.ajax({
+                url: "../db/teamremove.php?id=" + $(this).find("#teamright").attr("team_id")
+            }).done(removereturned).fail(removereturned);
+            // wenn das rechte team höhere punkte als das linke hat
+        } else if ($(this).find("#teamleft").val() < $(this).find("#teamright").val()) {
+            removereq++;
+            teamsremaining++;
+            // wird das linke entfernet
+            $.ajax({
+                url: "../db/teamremove.php?id=" + $(this).find("#teamleft").attr("team_id")
+            }).done(removereturned).fail(removereturned);
+        } else {
+            teamsremaining++;
+        }
+    });
+    init();
+    if (teamsremaining > 2) {
+        teamsremaining = 0;
+    }
+});
+
+function removereturned() {
+    removereq--;
+}
